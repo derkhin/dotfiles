fa5106ec92233263fab816ef578cc94487e35f94
derkhin <mr.sayyedirfan@gmail.com>
2026-04-04 17:31:17 +0530

    zsh_config

--- run_onchange_install-packages.sh
+++ run_onchange_install-packages.sh
@@ -1,2 +1,14 @@
 #!/bin/sh
-sudo pacman -Syu --noconfirm zsh kitty hyprlock
+sudo pacman -Syu --noconfirm zsh kitty hyprlock waybar hyprpaper
+
+# To install yay uncomment below lines and run chezmoi apply
+
+#sudo pacman -S --needed --noconfirm base-devel
+#git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si && rm -rf ~/yay
+
+#oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+#zsh_theme 
+yay -S --noconfirm zsh-theme-powerlevel10k-git
+echo 'source /usr/share/zsh-theme-powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
